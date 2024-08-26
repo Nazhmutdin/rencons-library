418e526725cb02fa0335661596503873c91d864f
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2024-08-26 11:35:45 +0300

    fixing base model count method realization

--- naks_library/base_model.py
+++ naks_library/base_model.py
@@ -9,7 +9,7 @@ class CRUDMixin:
 
     @classmethod
     async def get(cls, conn: AsyncSession, ident: uuid.UUID | str):
-        stmt = cls._dump_get_stmt(ident)
+        stmt = cls.dump_get_stmt(ident)
         response = await conn.execute(stmt)
         result = response.scalar_one_or_none()
         return result
@@ -17,7 +17,7 @@ class CRUDMixin:
 
     @classmethod
     async def get_many(cls, conn: AsyncSession, expression: sa.ColumnExpressionArgument, limit: int, offset: int):
-        stmt = cls._dump_get_many_stmt(expression)
+        stmt = cls.dump_get_many_stmt(expression)
 
         amount = await cls.count(conn, expression)
 
@@ -36,7 +36,7 @@ class CRUDMixin:
 
     @classmethod
     async def create(cls, data: list[dict], conn: AsyncSession):
-        stmt = cls._dump_create_stmt(
+        stmt = cls.dump_create_stmt(
             data
         )
 
@@ -45,63 +45,69 @@ class CRUDMixin:
 
     @classmethod
     async def update(cls, conn: AsyncSession, ident: uuid.UUID | str, data: dict[str, t.Any]):
-        stmt = cls._dump_update_stmt(ident, data)
+        stmt = cls.dump_update_stmt(ident, data)
         await conn.execute(stmt)
 
 
     @classmethod
     async def delete(cls, conn: AsyncSession, ident: uuid.UUID | str):
-        stmt = cls._dump_delete_stmt(ident)
+        stmt = cls.dump_delete_stmt(ident)
         await conn.execute(stmt)
 
 
     @classmethod
     async def count(cls, conn: AsyncSession, expression: sa.ColumnExpressionArgument | None = None):
-        if isinstance(expression, sa.ColumnElement):
+    
+        if expression is not None:
 
-            stmt = sa.select(sa.func.count()).select_from(cls).where(expression)
-
-            return (await conn.execute(stmt)).scalar_one()
+            stmt = cls.get_base_count_stmt(expression).where(expression).distinct()
 
         else:
-            return (await conn.execute(sa.select(sa.func.count()).select_from(cls).distinct())).scalar_one()
+            stmt = cls.get_base_count_stmt(expression).distinct()
+
+        return (await conn.execute(stmt)).scalar_one()
 
 
     @classmethod
-    def _get_column(cls, ident: str | uuid.UUID):
+    def get_column(cls, ident: str | uuid.UUID):
         return sa.inspect(cls).primary_key[0]
 
 
     @classmethod
-    def _dump_create_stmt(cls, data: list[dict[str, t.Any]]):
+    def dump_create_stmt(cls, data: list[dict[str, t.Any]]):
         return sa.insert(cls).values(
             data
         )
 
 
     @classmethod
-    def _dump_get_stmt(cls, ident: str | uuid.UUID):
+    def dump_get_stmt(cls, ident: str | uuid.UUID):
         return sa.select(cls).where(
-            cls._get_column(ident) == ident
+            cls.get_column(ident) == ident
         )
 
 
     @classmethod
-    def _dump_get_many_stmt(cls, expression: sa.ColumnExpressionArgument):
+    def dump_get_many_stmt(cls, expression: sa.ColumnExpressionArgument):
         return sa.select(cls).filter(expression)
     
 
     @classmethod
-    def _dump_update_stmt(cls, ident: str | uuid.UUID, data: dict[str, t.Any]):
+    def dump_update_stmt(cls, ident: str | uuid.UUID, data: dict[str, t.Any]):
         return sa.update(cls).where(
-            cls._get_column(ident) == ident
+            cls.get_column(ident) == ident
         ).values(
             **data
         )
 
 
     @classmethod
-    def _dump_delete_stmt(cls, ident: str | uuid.UUID):
+    def dump_delete_stmt(cls, ident: str | uuid.UUID):
         return sa.delete(cls).where(
-            cls._get_column(ident) == ident
+            cls.get_column(ident) == ident
         )
+    
+
+    @classmethod
+    def get_base_count_stmt(cls, expression: sa.ColumnExpressionArgument | None):
+        return sa.select(sa.func.count()).select_from(cls)
