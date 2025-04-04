fd86e150cf21cfabdeb721dc01491bd79267ebaf
Nazhmutdin <gumuevnazhmutdin1248@gmail.com>
2025-04-04 13:03:21 +0300

    feat: add ApplicationException, InvalidNationException and InvalidProjectException

--- rencons_library/exc/main.py
+++ rencons_library/exc/main.py
@@ -3,3 +3,21 @@ class AccessForbiddenException(Exception):
     def __init__(self, message: str, code: str = "access_forbidden") -> None:
         self.code = code
         self.message = message
+
+
+class ApplicationException(Exception):
+    def __init__(self, message: str, code: str = "application_error") -> None:
+        self.code = code
+        self.message = message
+
+
+class InvalidProjectException(Exception):
+    def __init__(self, message: str, code: str = "invalid_project") -> None:
+        self.code = code
+        self.message = message
+
+
+class InvalidNationException(Exception):
+    def __init__(self, message: str, code: str = "invalid_nation") -> None:
+        self.code = code
+        self.message = message
